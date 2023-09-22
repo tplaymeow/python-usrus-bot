559fd72a86147a9ee98af83ee7b9903608e92c7e
Vadim Aleshin <mov4d@MacBook-Air-Vadim.local>
2023-09-22 20:00:10 +0300

    voice count obscene

--- python_usrus_bot/bot/handle_obscene_stat.py
+++ python_usrus_bot/bot/handle_obscene_stat.py
@@ -9,8 +9,8 @@ from python_usrus_bot.text_utills.obscene_words import obscene_words_count
 from python_usrus_bot.text_utills.text_processing import text_preprocessing
 
 
-async def handle_obscene_stat(message: Message, context: BotContext) -> None:
-    message_text = message.text
+async def handle_obscene_stat(message: Message, context: BotContext, text: str = None) -> None:
+    message_text = text or message.text
     if message_text is None:
         return
 
